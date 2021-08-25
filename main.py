0caa345284d384c2986908c4e169aa541b8f5c08
Sibints2000 <sibints2000@gmail.com>
2021-08-25 20:30:50 +0530

    Twenty Eighth Commit

--- main.py
+++ main.py
@@ -63,6 +63,9 @@ def save():
             website_entry.delete(0, END)
             password_entry.delete(0, END)
 
+# ---------------------------- FIND PASSWORD ------------------------------- #
+def find_password():
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -94,7 +97,7 @@ password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1)
 
 # Buttons
-search_button = Button(text="Search", width=13)
+search_button = Button(text="Search", width=13, command=find_password)
 search_button.grid(row=1, column=2)
 generate_password_button = Button(text="Generate Password", command=generate_password)
 generate_password_button.grid(row=3, column=2)

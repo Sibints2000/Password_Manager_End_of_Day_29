4d6e796750cdbc966e8f59f069397d1074fb0235
Sibints2000 <sibints2000@gmail.com>
2021-08-25 20:10:18 +0530

    Seventeenth Commit

--- main.py
+++ main.py
@@ -45,13 +45,13 @@ def save():
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Oops", message="Please make sure you haven't left any fields empty.")
     else:
-        with open("data.json", "w") as data_file:
+        with open("data.json", "r") as data_file:
             # Reading old data
             data = json.load(data_file)
             # Updating old data with new data
             data.update(new_data)
 
-        with open("data.json", "r") as data_file:
+        with open("data.json", "w") as data_file:
             # Saving updated data
             json.dump(new_data, data_file, indent=4)
 

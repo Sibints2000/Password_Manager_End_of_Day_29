b955c3ac11f6f1c0c05dd44b301e8fcd000c34df
Sibints2000 <sibints2000@gmail.com>
2021-08-25 20:16:58 +0530

    Twenty Second Commit

--- main.py
+++ main.py
@@ -56,10 +56,10 @@ def save():
             # Updating old data with new data
             data.update(new_data)
 
-        with open("data.json", "w") as data_file:
-            # Saving updated data
-            json.dump(new_data, data_file, indent=4)
-
+            with open("data.json", "w") as data_file:
+                # Saving updated data
+                json.dump(new_data, data_file, indent=4)
+        finally:
             website_entry.delete(0, END)
             password_entry.delete(0, END)
 

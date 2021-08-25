6a9c8599efa5081a8a3f31874a79d0ad3a34996e
Sibints2000 <sibints2000@gmail.com>
2021-08-25 20:36:49 +0530

    Thirty Seventh Commit

--- main.py
+++ main.py
@@ -77,6 +77,7 @@ def find_password():
 
 # ---------------------------- UI SETUP ------------------------------- #
 
+
 window = Tk()
 window.title("Password Manager")
 window.config(padx=50, pady=50)

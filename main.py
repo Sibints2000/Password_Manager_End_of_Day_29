53e39b07ea05b2b62e542b62ee79cd25dc9916dd
Sibints2000 <sibints2000@gmail.com>
2021-08-25 19:06:35 +0530

    Fourteenth Commit

--- main.py
+++ main.py
@@ -50,8 +50,11 @@ def save():
             data = json.load(data_file)
             # Updating old data with new data
             data.update(new_data)
+
+        with open("data.json", "w") as data_file:
             # Saving updated data
             json.dump(new_data, data_file, indent=4)
+
             website_entry.delete(0, END)
             password_entry.delete(0, END)
 
